aebadcc6cbf6311b427001e27697378ff9cf0cdb
Dft9380 <93413366+Dft9380@users.noreply.github.com>
2022-03-04 03:44:15 -0300

    la_eterna_verdad

---
+++ Curso /Kotlin /Ejercicios/la_eterna_verdad.kt
@@ -0,0 +1,24 @@
+Escriba un programa que imprima la cadena "2 + 2 = 4" (sin comillas).
+
+Utilice la plantilla proporcionada para su solución. No agregue espacios adicionales a su cadena.
+
+Salida de muestra 
+
+2 + 2 = 4
+
+------------------------------------------------------------------------
+  
+  fun main() {
+    val total = 2 + 2
+    println("2 + 2 = " + total)
+  }
+  
+--------------------------------------------------------------------------
+  
+  fun main() = println("2 + 2 = 4")
+  
+--------------------------------------------------------------------------
+  
+  fun main() {
+    println("2 + 2 = 4")
+  }
